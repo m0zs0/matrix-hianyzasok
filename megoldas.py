df2b7d703397b1ccffeb14f4466fe36d3c048e42
misk0lc <126462776+misk0lc@users.noreply.github.com>
2023-03-06 09:25:40 +0100

    fájlbeolvasás kész

---
+++ megoldas.py
@@ -0,0 +1,13 @@
+#0. feladat A hianyzasok.txt beolvasása listában listákba
+
+hianyzasok=[]
+with open("./adatok/hianyzasok.txt","r",encoding="utf-8") as fm:
+    for sor in fm:
+        seged_lista=(sor.strip().split(","))
+        # l=[]
+        # for szam in seged_lista:
+        #     l.append(int(szam))
+        # hianyzasok.append(l)
+        hianyzasok.append(list(map(int, seged_lista)))
+
+print(hianyzasok)
