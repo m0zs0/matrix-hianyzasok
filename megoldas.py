3a8d49a0b4e08502814abdc3c6445499d13c561b
misk0lc <126462776+misk0lc@users.noreply.github.com>
2023-03-09 10:14:36 +0100

    3. feladat hotfix

--- megoldas.py
+++ megoldas.py
@@ -21,14 +21,23 @@ for het in hianyzasok:
 print(f"1. feladat: {osszeg} óra hiányzás volt összesen")
 
 # 2. Volt-e olyan hét, amikor nem volt hiányzó?
-print(f"2. feladat: Nem volt olyan hét, amikor nem volt hiányzó")
 
 # 3. Volt-e olyan hét, amikor ötnél kevesebb hiányzás volt
-volt_e_otnel_kevesebb_hianyzas=False
-for het in hianyzasok:
-    if sum(het)<5:
-        volt_e_otnel_kevesebb_hianyzas=True
-if volt_e_otnel_kevesebb_hianyzas==True:
+
+index=0
+while index<len(hianyzasok) and not (sum(hianyzasok[index])<5):
+    index+=1
+van=index<len(hianyzasok)
+if van:
     print(f"3. feladat: Volt olyan hét, amikor ötnél kevesebb hiányzó volt")
 else:
     print(f"3. feladat: Nem volt olyan hét, amikor ötnél kevesebb hiányzó volt")
+
+# volt_e_otnel_kevesebb_hianyzas=False
+# for het in hianyzasok:
+#     if sum(het)<5:
+#         volt_e_otnel_kevesebb_hianyzas=True
+# if volt_e_otnel_kevesebb_hianyzas==True:
+#     print(f"3. feladat: Volt olyan hét, amikor ötnél kevesebb hiányzó volt")
+# else:
+#     print(f"3. feladat: Nem volt olyan hét, amikor ötnél kevesebb hiányzó volt")
