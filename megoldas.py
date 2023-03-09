b2439df26f1fcefe3edf93a64eb3cde4bc05ebc7
mozso <126395568+m0zs0@users.noreply.github.com>
2023-03-09 09:42:54 +0100

    2. feladat kiírása megtörtént

--- megoldas.py
+++ megoldas.py
@@ -19,3 +19,6 @@ for het in hianyzasok:
     osszeg+=sum(het)
 
 print(f"1. feladat: {osszeg} óra hiányzás volt összesen")
+
+# 2. Volt-e olyan hét, amikor nem volt hiányzó?
+print(f"2. feladat: Nem volt olyan hét, amikor nem volt hiányzó")
