998b9893edf7f4041cc40ec9fc21324574b8ac59
mozso <126395568+m0zs0@users.noreply.github.com>
2023-03-09 11:14:14 +0100

    kész

--- megoldas.py
+++ megoldas.py
@@ -17,19 +17,46 @@ print(hianyzasok)
 osszeg=0
 for het in hianyzasok:
     osszeg+=sum(het)
-
-# 1. Hány óra hiányzás volt összesen?
 print(f"1. feladat: {osszeg} óra hiányzás volt összesen")
 
-# 3. Volt-e olyan hét, amikor ötnél kevesebb hiányzás volt
-print("3. feladat:", end="")
-
-for het in hianyzasok:
-    if sum(het)<5:
-        print(f" Volt olyan hét, amikor ötnél kevesebb hiányzó volt")
-    else:
-        print("Nem volt olyan hét, amikor ötnél kevesebb hiányzó volt")
-
-
 # 2. Volt-e olyan hét, amikor nem volt hiányzó?
-print(f"2. feladat: Nem volt olyan hét, amikor nem volt hiányzó")
+index=0
+while index<len(hianyzasok) and not(sum(hianyzasok[index])==0):
+    index+=1
+van=index<len(hianyzasok)
+if van:
+    print(f"2. feladat: Volt olyan hét, amikor nem volt hiányzó")
+else:    
+    print(f"2. feladat: Nem volt olyan hét, amikor nem volt hiányzó")
+
+# 3. Volt-e olyan hét, amikor ötnél kevesebb hiányzás volt
+index=0
+while index<len(hianyzasok) and not(sum(hianyzasok[index])<5):
+    index+=1
+if index<len(hianyzasok):
+    print(f"3. feladat: Volt olyan hét, amikor ötnél kevesebb hiányzó volt")
+else:
+    print("3. feladat: Nem volt olyan hét, amikor ötnél kevesebb hiányzó volt")
+
+# 4. Melyik héten volt a legtöbb hiányzás?
+max_index=0
+for index in range(len(hianyzasok)):
+    if sum(hianyzasok[index])>sum(hianyzasok[max_index]):
+        max_index=index
+
+print(f"4. feladat: A legtöbb hiányzás a {max_index+1}. héten volt ({sum(hianyzasok[max_index])})")
+
+# 5. Hányadik héten volt egyetlen hiányzás?
+index=0
+while index<len(hianyzasok) and not(sum(hianyzasok[index])==1):
+    index+=1
+
+van=index<len(hianyzasok)
+if van:
+    sorszam=index
+    print(f"5. feladat: {sorszam+1}. héten volt egyetlen hiányzás")
+else:
+    sorszam=-1
+    print(f"5. feladat: egyik héten sem volt egyetlen hiányzás")
+
+
