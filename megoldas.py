14b08fc448dbbcd9d85f63007f6bb8abbb67dfca
misk0lc <126462776+misk0lc@users.noreply.github.com>
2023-03-09 08:15:45 +0100

    új sor beszúrása

--- megoldas.py
+++ megoldas.py
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
 #0. feladat A hianyzasok.txt beolvasása listában listákba
 
 hianyzasok=[]
 with open("./adatok/hianyzasok.txt","r",encoding="utf-8") as fm:
     for sor in fm:
         seged_lista=(sor.strip().split(","))
-=======
-#0. feladat A hianyzasok.txt beolvasásása listában listákba
-#állománybeolvasás kész
-hianyzasok=[]
-with open("./adatok/hianyzasok.txt","r",encoding="utf8") as fm:
-    for sor in fm:
-        seged_lista=sor.strip().split(",")
->>>>>>> 99f87eb99c4d1e2f59e710c929e166b3c3956e7d
         # l=[]
         # for szam in seged_lista:
         #     l.append(int(szam))
         # hianyzasok.append(l)
         hianyzasok.append(list(map(int, seged_lista)))
 
+print("a beolvasott mátrix.")
 print(hianyzasok)
