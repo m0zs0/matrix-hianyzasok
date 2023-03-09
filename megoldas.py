968ed11281c0722274a0f2f76a5c2708fa6248d0
mozso <126395568+m0zs0@users.noreply.github.com>
2023-03-09 08:12:37 +0100

    Új sor beszúrása megtörtént

--- megoldas.py
+++ megoldas.py
@@ -10,4 +10,5 @@ with open("./adatok/hianyzasok.txt","r",encoding="utf8") as fm:
         # hianyzasok.append(l)
         hianyzasok.append(list(map(int, seged_lista)))
 
+print("a beolvasott mátrix: ")
 print(hianyzasok)
