f1332b9546374af402b6fe58605f2ff64d30bc2f
mozso <126395568+m0zs0@users.noreply.github.com>
2023-03-09 08:34:34 +0100

    kész 1. feladat

--- megoldas.py
+++ megoldas.py
@@ -13,16 +13,9 @@ with open("./adatok/hianyzasok.txt","r",encoding="utf-8") as fm:
 print("a beolvasott mátrix: ")
 print(hianyzasok)
 
-<<<<<<< HEAD
 # 1. Hány óra hiányzás volt összesen?
-=======
->>>>>>> 2254e0cf4d5caf26a2f030c1bd611d1d316dd260
 osszeg=0
 for het in hianyzasok:
     osszeg+=sum(het)
 
-<<<<<<< HEAD
-=======
-# 1. Hány óra hiányzás volt összesen?
->>>>>>> 2254e0cf4d5caf26a2f030c1bd611d1d316dd260
 print(f"1. feladat: {osszeg} óra hiányzás volt összesen")
